03c7947398f3cc96888ca0078b58c6b693d5946b
ogazboiz <ogazboizakpolo@gmail.com>
2024-11-22 23:14:43 +0100

    done with the bugging

--- vite.config.js
+++ vite.config.js
@@ -2,22 +2,8 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 
-console.log("Alias resolves to:", path.resolve(__dirname, './src'));
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
-  build: {
-    rollupOptions: {
-      output: {
-        entryFileNames: '[name].js',
-      },
-    },
-  },
-  logLevel: 'debug', // Add this line for detailed logs
   
 });
